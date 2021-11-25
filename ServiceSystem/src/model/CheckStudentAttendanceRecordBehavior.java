a43e90eb7cc51f4166a93deefb9b1b84b3cd5b3b
Tiered-190 <58621164+Tiered-190@users.noreply.github.com>
2021-11-25 12:24:43 +0800

    Delete ServiceSystem directory

--- ServiceSystem/src/model/CheckStudentAttendanceRecordBehavior.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface CheckStudentAttendanceRecordBehavior {
-	//查看考勤记录
-	public ArrayList<ArrayList<String>> checkStudentAttendanceRecord(int studentID);
-}
--- ServiceSystem/src/model/CheckStudentTrackInfoBehavior.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface CheckStudentTrackInfoBehavior {
-	//查看成长档案
-	public ArrayList<String> checkStudentTrackInfo(int studentID);
-}
