370e88609ebca2e70692cf7c7fe3d3e4ce285b1d
wWuZhaoTong <1192840289@qq.com>
2021-11-28 16:09:18 +0800

    Delete Affairs System/model directory

--- Affairs System/model/Dept.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Dept extends Person{
-	DatabaseOperation DatabaseOperation;
-	
-	public void setDatabaseOperation(DatabaseOperation DatabaseOperation) {
-		this.DatabaseOperation = DatabaseOperation;
-	}
-}
