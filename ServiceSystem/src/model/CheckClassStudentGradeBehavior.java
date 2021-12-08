41b0be9e9438d1a141354865b2657561703ea2d4
aphlete <90031965+aphlete@users.noreply.github.com>
2021-12-08 22:56:31 +0800

    更新教师端

--- ServiceSystem/src/model/CheckClassStudentGradeBehavior.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface CheckClassStudentGradeBehavior {
-	//查看班级学生成绩
-	
-	public ArrayList<ArrayList<String>> checkclassstudentgrade(String teacherid, String examtime, String examtype);
-}
