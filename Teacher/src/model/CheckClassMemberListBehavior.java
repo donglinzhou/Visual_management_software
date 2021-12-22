7cca691c39f835280b0838c00b7bde68a66bdf9b
aphlete <90031965+aphlete@users.noreply.github.com>
2021-12-22 21:19:12 +0800

    Delete Teacher directory

--- Teacher/src/model/CheckClassMemberListBehavior.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface CheckClassMemberListBehavior {
-	//查看班级学生名单
-	
-	public ArrayList<String> checkclassmemberlist(String teacherid);
-}
--- Teacher/src/model/CheckStudentGradePrecentageBehavior.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public interface CheckStudentGradePrecentageBehavior {
-	//查看学科成绩分段占比表
-	//我理解不了啥意思，所以先不动
-	
-	public void checkstudentgradeprecentage();
-}
